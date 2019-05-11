4753986e4f32bb641c418eb5b1389468837eab2d
Joachim Goedhart <j.goedhart@uva.nl>
2019-05-11 21:43:31 +0200

    First commit

---
+++ EC_by_Strickler-Berg.R
@@ -0,0 +1,56 @@
+require(caTools)
+
+# Read a file with the columns in the order 1:Wavelength [nm], 2:Absorbance and 3:Emission
+df <- read.csv("mTurquoise2_spectra.csv")
+
+#Set some protein specific parameters
+EC = NA
+QY = 0.93
+tau = 4.00
+
+#Set some physical parameters
+#Refractive Index
+n_cells = 1.365
+n_H2O = 1.33
+Pi = 3.141593
+
+#speed of light in cm/s
+c = 3e10
+Navogadro = 6.022e23
+
+#Calculate constant
+constant = 8*2303*Pi*c/Navogadro
+
+#Read wavelength data
+lambda <- df$lambda
+
+#Convert wavelength (nm) to wavenumber (cm-1)
+wv <- 1/(lambda*1e-7)
+
+#Replace missing values by zero
+df[is.na(df)] <- 0
+
+#Read Abs/Em data
+Abs <- df$Abs_mTq2
+Em <- df$Em_mTq2
+
+#Calculate integrals
+Em3 <- Em/wv^3
+Int_I <-  trapz(wv,Em)/trapz(wv,(Em/wv^3))
+Int_A <-  abs(trapz(wv,(Abs/wv)))
+
+#Calculate the kr/EC
+k <- constant*n_H2O^2*Int_I*Int_A
+
+# Calculate Extinction Coefficient [M-1 cm-1]
+EC <-  QY * 1e9/k/tau
+EC
+
+# Uncomment to calculate Quantum Yield, requires EC and tau to be known
+# QY <- EC*k*tau*1e-9
+# QY
+
+# Uncomment to calculate the fluorescence lifetime [ns], requires EC and QY to be known
+# tau <- QY * 1e9/k/EC
+# tau
+
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Strickler_Berg
+
+The extinction coefficient of a fluorophore can be estimated from the Strickler-Berg equation ([Strickler and Berg, 1962](https://doi.org/10.1063/1.1733166))
+
+The R-script EC_by_Strickler-Berg.R demonstrates the calculation of the EC of mTurquoise2 based on these spectroscopic data:
+
+* Absorbance/emission spectra, taken from the file 'mTurquoise2_spectra.csv' 
+* Quantum Yield: 0.93
+* Fluorescence lifetime: 4 [ns]
+
+
+
+
+
+
+
