015bb6109ba55b2ebca600e9889d3dd9c726b5f7
Joachim Goedhart <j.goedhart@uva.nl>
2019-05-12 20:19:08 +0200

    Updated script
    -minor changes to the layout

--- EC_by_Strickler-Berg.R
+++ EC_by_Strickler-Berg.R
@@ -1,9 +1,15 @@
-require(caTools)
+# R-script that calculates spectroscopic parameters of a fluorophore using the Strickler-Berg equation 
+# Strickler and Berg, 1992: https://doi.org/10.1063/1.1733166
+# This script can be used for calculation of Extinction coefficient (EC), for the case where
+# Quantum Yield (QY) and fluorescence lifetime (tau) are known
+# By: Joachim Goedhart and Marten Postma - 2019
 
-# Read a file with the columns in the order 1:Wavelength [nm], 2:Absorbance and 3:Emission
+require(caTools)  #Needed for trapz()
+
+# Read a csv file where the columns have the data of respectively 1:Wavelength [nm], 2:Absorbance and 3:Emission
 df <- read.csv("mTurquoise2_spectra.csv")
 
-#Set some protein specific parameters
+#Set the known spectroscopic parameters for the fluorophore
 EC = NA
 QY = 0.93
 tau = 4.00
@@ -13,9 +19,7 @@ tau = 4.00
 n_cells = 1.365
 n_H2O = 1.33
 Pi = 3.141593
-
-#speed of light in cm/s
-c = 3e10
+c = 3e10 #speed of light in cm/s
 Navogadro = 6.022e23
 
 #Calculate constant
@@ -42,10 +46,6 @@ Int_A <-  abs(trapz(wv,(Abs/wv)))
 #Calculate the kr/EC
 k <- constant*n_H2O^2*Int_I*Int_A
 
-# Calculate Extinction Coefficient [M-1 cm-1]
-EC <-  QY * 1e9/k/tau
-EC
-
 # Uncomment to calculate Quantum Yield, requires EC and tau to be known
 # QY <- EC*k*tau*1e-9
 # QY
@@ -53,4 +53,9 @@ EC
 # Uncomment to calculate the fluorescence lifetime [ns], requires EC and QY to be known
 # tau <- QY * 1e9/k/EC
 # tau
+
+# Calculate Extinction Coefficient [M-1 cm-1]
+EC <-  QY * 1e9/k/tau
+EC
+
 
